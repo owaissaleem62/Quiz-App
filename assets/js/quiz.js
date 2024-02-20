5fdc1909aed29df8163ef74725f350c4497ac632
Owais Saleem <saleemowais62@gmail.com>
2024-02-20 15:18:58 +0500

    Quiz App

---
+++ assets/js/quiz.js
@@ -0,0 +1,176 @@
+let quiz_rules = [
+    "You Have Only 20 Seconds For Each Question.",
+    
+    "Once You Select Any Answer.It Can't Be Undone",
+    
+    "You'll Get Points On The Basic Of Your Correct Answers."
+];
+
+
+let quiz_questions = [
+    // questions 
+    
+
+
+
+    {
+        // question
+        question: "Who is the father of HTML ?",
+
+        // options
+        options : [
+            "Rasmus Lerdorf",
+            "Tim Berners-Lee",
+            "Brendan Eich",
+            "Sergey Brin",
+        ],
+
+        // correct answer
+        answer: 2,
+    },
+
+    {
+        // question
+        question: "What does HTML stand for?",
+
+        // options
+        options : [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Text Making Language",
+        ],
+
+        // correct answer
+        answer: 1,
+    },
+    
+   
+
+
+    {
+        // question
+        question: "The property in CSS used to change the background color of an element is",
+
+        // options
+        options : [
+            "bgcolor",
+            "color",
+            "background-color",
+            "All of the above",
+        ],
+
+        // correct answer
+        answer: 3,
+    },
+
+
+    
+
+    {
+        // write yout question here
+        question: "What does CSS stand for?",
+    
+        options: [
+            "Computer Style Sheet",
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Createive Style Sheets",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+
+    {
+        // write yout question here
+        question: "What is a correct syntax to output 'Hello World' in C++?",
+    
+        options: [
+            'System.out.printLn("Hello World")',
+            'cout << "Hello World"',
+            'print("Hello World")',
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+
+    {
+        // write yout question here
+        question: "What does SQL stand for?",
+    
+        options: [
+            "Strong Question Language",
+            "Structure Query Language",
+            "Structured Question Language",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+
+    {
+        // write yout question here
+        question: "What does PHP stand for?",
+    
+        options: [
+            "Private Homepage   ",
+            "Personal Hypertext Processor",
+            "PHP: Hypertext Preprocessor",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+
+    {
+        // write yout question here
+        question: "Inside which HTML element do we put the JavaScript?",
+    
+        options: [
+            "javascript ",
+            "scripting",
+            "js",
+            "script",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 4,
+    //    selected_answer:null
+    },
+   
+
+   
+
+   
+
+
+
+
+    {
+        // write yout question here
+        question: "Npm Stands For ?",
+    
+        options: [
+            'Note Package Manager',
+            'Nine Package Manager',
+            'Node Package Manager',
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+
+    {
+        // write yout question here
+        question: "Which of the following is correct?",
+    
+        options: [
+            "JQuery is a JavaScript Library",
+            "JQuery is a JSON Library",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+];
